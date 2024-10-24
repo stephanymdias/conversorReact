628129eec6a4c604f7c33407810d3851046d5674
Stephany Marciano Dias <130869859+stephanymdias@users.noreply.github.com>
2024-10-24 19:15:03 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ConversorDeMoedas from './components/ConversorDeMoedas'
+
+function App() {
+  return(
+    <div className='App'>
+      <ConversorDeMoedas/>
+    </div>
+  )
+}
+
+export default App;
